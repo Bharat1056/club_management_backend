df668e574396db14ed9fbdd480dc07395060861e
Bharat Panigrahi <111682310+Bharat1056@users.noreply.github.com>
2025-01-05 21:25:07 +0530

    Update query.middleware.js

--- middleware/query.middleware.js
+++ middleware/query.middleware.js
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose';
-import { defaultPaginateValue } from '../config/paginate.config.js';
+import { defaultPaginateValue } from "../constants/constant.js";
 const queryMiddleware = (Model, modelPath) => async (req, res, next) => {
   try {
     const {
