4d246a2016baf39656ab8e8b00eeca273cd9cea9
Bharat Panigrahi <111682310+Bharat1056@users.noreply.github.com>
2025-01-05 21:16:57 +0530

    Update constant.js

--- constants/constant.js
+++ constants/constant.js
@@ -3,7 +3,7 @@ export const clubPhotoLimit = 3;
 export const higherMemberLimit = 2;
 export const hashSaltRound = 10;
 export const defaultPaginateValue = 20;
-export const resendDeliverUser = "Acme <onboarding@resend.dev>"
+export const resendDeliverUser = "imbitu@bharatpanigrahi.cloud"
 export const domainList = [
   "web",
   "app",
